2608d68143e37eaad50661e2ac67d027a4f3e8f4
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-14 20:02:05 +0530

    Update user_utils.js

--- Backend/utils/user_utils.js
+++ Backend/utils/user_utils.js
@@ -6,7 +6,7 @@ export const sendcookie = (user, res, message, statuscode = 200) => {
     .status(statuscode)
     .cookie("token", token, {
       httpOnly: true,
-      domain: "https://inventory-management-eqq8jjf2x-paras-patels-projects-ebd7dde8.vercel.app"
+      domain: "https://inventory-management-eqq8jjf2x-paras-patels-projects-ebd7dde8.vercel.app",
       maxAge: 30 * 60 * 10000,
       sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "development" ? false : true,
