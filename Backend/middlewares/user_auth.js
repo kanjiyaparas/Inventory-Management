2157ca37f3895327453f87051ae0a90a10ea75da
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-14 22:36:07 +0530

    Update user_auth.js

--- Backend/middlewares/user_auth.js
+++ Backend/middlewares/user_auth.js
@@ -2,18 +2,26 @@ import User from "../models/user_model.js";
 import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
-  // const { token } = req.cookies;
-  const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
-  console.log("token", token);
-  if (!token) {
+  const cookie = req.headers.cookie;
+
+  if (!cookie || !cookie.startsWith('token=')) {
     return res.status(400).json({
       success: false,
-      message: "user is not found or Login first",
+      message: "User not found or Login first",
     });
   }
 
-  const decode = jwt.verify(token, process.env.SECRET_KEY);
+  // Extract the token part from 'token=<jwt_token>'
+  const token = cookie.split('=')[1];
 
-  req.user = await User.findById(decode._id);
-  next();
+  try {
+    const decode = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = await User.findById(decode._id); // Assuming the token contains user ID
+    next(); // Proceed to the next middleware
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token or authentication failed",
+    });
+  }
 };
